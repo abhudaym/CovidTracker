f91648171f60697a029f4194f5450de6611aafd4
Abhuday Mishra <abhuday@Abhudays-MacBook-Pro.local>
2020-06-13 01:22:17 +0530

    Footer change

--- src/World/components/Footer/Footer.jsx
+++ src/World/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import styles from './Footer.module.css';
 function Footer(){
     return (
 <div className={styles.footer}>
-    <p><em>Made with ❤️ by <a href="https://www.linkedin.com/company/aakash-research-labs/">Aakash Research Lab</a></em></p>
+    <p><em>Made with ❤️ by <a href="https://www.linkedin.com/company/aakash-research-labs/">Aakash Research Labs</a></em></p>
 </div> 
 );
 }
